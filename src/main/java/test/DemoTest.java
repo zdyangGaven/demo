e2893237b78b5f70187a64883c147ff53f1cfa79
zdyangGaven <yang1986jl@163.com>
2019-04-02 10:15:38 +0800

    本地仓库

--- src/main/java/test/DemoTest.java
+++ src/main/java/test/DemoTest.java
@@ -4,7 +4,7 @@ public class DemoTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("CCCC");
+       System.out.println("±¾µØ²Ö¿â");
 	}
 
 }
